7088aecb58a5ba7caedc6a099f4b5c3d28628a6c
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-04-19 21:11:33 +0900

    Initial Commit

---
+++ SlideshowApp_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp_4
+//
+//  Created by Kosuke Miyazaki on 2024/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
