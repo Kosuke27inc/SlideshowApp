d0eb21d8bfac0257f3e86a56c70ea2f9fc449587
Kosuke Miyazaki <27inc.kosukemiyazaki@gmail.com>
2024-04-20 20:36:52 +0900

    遂に完成！

--- SlideshowApp_4/ViewController.swift
+++ SlideshowApp_4/ViewController.swift
@@ -25,17 +25,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var playStopButton: UIButton!
 
     var images: [UIImage] = [
-        UIImage(named: "DALL-E_apple.png")!,
-        UIImage(named: "DALL-E_fox.png")!,
-        UIImage(named: "DALL-E_samurai.png")!
+        UIImage(named: "Lesson 5課題用画像/DALL-E_apple.png")!,
+        UIImage(named: "Lesson 5課題用画像/DALL-E_fox.png")!,
+        UIImage(named: "Lesson 5課題用画像/DALL-E_samurai.png")!
     ]
+
     var currentImageIndex = 0
     var timer: Timer?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        updateImageDisplay()
-    }
 
     func updateImageDisplay() {
         imageView.image = images[currentImageIndex]
@@ -54,12 +50,12 @@ class ViewController: UIViewController {
     @IBAction func playStopToggle(_ sender: UIButton) {
         if timer == nil {
             startSlideshow()
-            sender.setTitle("Stop", for: .normal)
+            sender.setTitle("停止", for: .normal)
             nextButton.isEnabled = false
             previousButton.isEnabled = false
         } else {
             stopSlideshow()
-            sender.setTitle("Play", for: .normal)
+            sender.setTitle("再生", for: .normal)
             nextButton.isEnabled = true
             previousButton.isEnabled = true
         }
@@ -69,14 +65,39 @@ class ViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImageAutomatically), userInfo: nil, repeats: true)
     }
 
-    func stopSlideshow() {
-        timer?.invalidate()
-        timer = nil
-    }
-
     @objc func changeImageAutomatically() {
         nextImage(self)
     }
+    
+    @objc func imageViewTapped(_ sender: UITapGestureRecognizer) {
+        if timer != nil {
+            stopSlideshow()
+        }
+        playStopButton.setTitle("再生", for: .normal)
+           nextButton.isEnabled = true
+           previousButton.isEnabled = true
+           performSegue(withIdentifier: "showDetail", sender: self)
+       }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTapGestureRecognizer()
+        updateImageDisplay()
+    }
+
+    func setupTapGestureRecognizer() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_:)))
+        imageView.addGestureRecognizer(tapGesture)
+        imageView.isUserInteractionEnabled = true
+    }
+
+func stopSlideshow() {
+    timer?.invalidate()
+    timer = nil
+    playStopButton.setTitle("再生", for: .normal)
+    nextButton.isEnabled = true
+    previousButton.isEnabled = true
+}
 }
 
 
